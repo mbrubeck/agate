f89d2179a9e3978d2c7d609e016e07b99ea0be69
Matt Brubeck <mbrubeck@limpet.net>
2020-05-17 08:35:12 -0700

    Use async-std

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,5 @@
-use {
-    agate::{Result},
-    rustls::{NoClientAuth, ServerConfig, ServerSession},
-    std::{
-        net::TcpListener,
-        sync::Arc,
-    },
-};
+use agate::Result;
 
 fn main() -> Result {
-    let tls_config = Arc::new(ServerConfig::new(NoClientAuth::new()));
-    // TODO: configure a certificate
-
-    let listener = TcpListener::bind("0.0.0.0:1965")?;
-    for stream in listener.incoming() {
-        let session = ServerSession::new(&tls_config);
-    }
     Ok(())
 }
