51b0749e5a566a2f945a1cbf9bd4a6aee421cd0c
w1kee <31793948+w1kee@users.noreply.github.com>
2023-07-07 15:49:42 +0200

    simplify config_attr condition
    target_os = "windows" -> windows
    
    Co-authored-by: Johann150 <johann@qwertqwefsday.eu>

--- src/main.rs
+++ src/main.rs
@@ -159,7 +159,8 @@ static ARGS: Lazy<Args> = Lazy::new(|| {
 
 struct Args {
     addrs: Vec<SocketAddr>,
-    #[cfg_attr(target_os = "windows", allow(dead_code))] // only used on unix, so dead code on windows
+    // only used on unix, so dead code on windows
+    #[cfg_attr(windows, allow(dead_code))]
     sockets: Vec<PathBuf>,
     content_dir: PathBuf,
     certs: Arc<certificates::CertStore>,
