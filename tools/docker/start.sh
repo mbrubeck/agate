f48ca0aaa1114935064c7b73f737243413aadc49
geraldwuhoo <gerald@geraldwu.com>
2024-11-20 17:41:44 -0800

    delete start.sh and update README

--- tools/docker/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Using hostname ${HOSTNAME:-localhost}"
-echo "Using lang ${LANG:-en-US}"
-
-exec agate --content /gmi/ \
-	--hostname "${HOSTNAME:-localhost}" \
-	--lang "${LANG:-en-US}"
